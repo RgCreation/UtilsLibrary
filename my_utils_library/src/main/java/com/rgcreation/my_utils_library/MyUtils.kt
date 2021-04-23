f0980e30dd0a52f70c100039e3707d72219bfa4c
sathish5131 <sathishrg5131@gmail.com>
2021-04-23 10:16:28 +0530

    Removed class

--- my_utils_library/src/main/java/com/rgcreation/my_utils_library/MyUtils.kt
+++ my_utils_library/src/main/java/com/rgcreation/my_utils_library/MyUtils.kt
@@ -5,12 +5,9 @@ import android.widget.Toast
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.format.DateTimeFormatter
 
-class MyUtils {
-
     fun LocalDateTime.exportFormat(): String =
         DateTimeFormatter.ofPattern("yyyy_MM_dd__kk_mm_ss").format(this)
 
     fun Context.myToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
-}
